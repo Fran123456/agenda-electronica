514bc5ffef5459c6a9a267344ef882c74aef8296
fran <navasfran98@gmail.com>
2018-12-02 00:52:40 -0600

    diseño de campana de notificación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,6 +109,39 @@
               <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
           </div>
               <ul class="nav navbar-top-links navbar-right">
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                        <i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-alerts">
+                        <li>
+                            <a href="mailbox.html">
+                                <div>
+                                    <i class="fa fa-envelope fa-fw"></i> You have 16 messages
+                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="profile.html">
+                                <div>
+                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
+                                    <span class="pull-right text-muted small">12 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </li>
+
+
+
+
+
+
                   <li>
                               <a href="{{ route('logout') }}"
                                   onclick="event.preventDefault();
