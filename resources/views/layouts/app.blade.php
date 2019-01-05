38fc1f99db3b069bc0a0e0bb194394dfd1004f78
fran <navasfran98@gmail.com>
2019-01-05 17:59:33 -0600

    modificaciones en header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{'yeti.png'}}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Yeti-Task</title>
 
     <!-- Styles -->
 
