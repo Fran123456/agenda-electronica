ccb8de899815af99a0e34ecde46e223a24b9d3bd
fran <navasfran98@gmail.com>
2019-01-05 18:06:12 -0600

    anexo de cambio contraseña a panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{'yeti.png'}}" />
+    <link rel="icon" type="image/png" href="{{ asset('/yeti.png')}}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -79,6 +79,8 @@
                               <li><a href="{{route('Perfil.show', Auth::user()->id) }}">Mi perfil</a></li>
                               <li class="divider"></li>
                               <li><a href="{{route('Perfil.edit', Auth::user()->id) }}">Edita perfil</a></li>
+                              <li class="divider"></li>
+                              <li><a href="{{route('actualizar-password', Auth::user()->id) }}">Cambio contraseña</a></li>
                               <li class="divider"></li>
                               <li>
                                 <a href="{{ route('logout') }}"
@@ -160,6 +162,7 @@
                       <ul class="nav nav-second-level collapse">
                           <li><a href="{{route('Perfil.show', Auth::user()->id) }}">Mi perfil</a></li>
                           <li><a href="{{route('Perfil.edit', Auth::user()->id) }}">Editar perfil</a></li>
+                          <li><a href="{{route('actualizar-password', Auth::user()->id) }}">Cambio contraseña</a></li>
                       </ul>
                   </li>
                   @endif
